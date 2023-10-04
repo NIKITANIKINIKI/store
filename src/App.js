a57f3831065d284741ea27df56b4f6ba714df707
Никита Комаров <nikitakmrv76@gmail.com>
2023-10-04 16:19:50 +0300

    added css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import React from 'react'
+import Items from "./components/Items";
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className='wrapper'>
-      <Header/>
-      <Footer/>
-    </div>
-  );
+  constructor(props){
+    super(props)
+    this.state={
+      items:[
+        {
+          id:1,
+          title: 'Table',
+          img: 'table.webp',
+          desc: "It's a beautiful table",
+          category:'tables',
+          price: '10000'
+        },
+        {
+          id:2,
+          title: 'Table',
+          img: 'white_table.jpg',
+          desc: "It's a beautiful table",
+          category:'tables',
+          price: '10000'
+        },
+        {
+          id:3,
+          title: 'Table',
+          img: 'table.webp',
+          desc: "It's a beautiful table",
+          category:'tables',
+          price: '10000'
+        },
+        {
+          id:4,
+          title: 'Table',
+          img: 'white_table.jpg',
+          desc: "It's a beautiful table",
+          category:'tables',
+          price: '10000'
+        },
+        
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className='wrapper'>
+        <Header/>
+        <Items items={this.state.items}/>
+        <Footer/>
+      </div>
+    )
+  }
+;
 }
 
 export default App;
